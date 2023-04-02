00db6abe1fc3117c7993703d209e18568d93b3a6
GabyFCE <duartegabriel2017@gmail.com>
2023-04-02 05:35:17 -0300

    Add project files.

---
+++ Ejercicios.Ejercicio8/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+while (true)
+{
+    Console.WriteLine("Quiere ingresar una frase? s/n");
+    string resp = Console.ReadLine();
+    if (resp == "s")
+    {
+        Console.WriteLine("Ingresa la frase!");
+        string frase = Console.ReadLine();
+        if (frase.Contains("fin"))
+        {
+            Console.WriteLine("la frase contiene 'fin'");
+            continue;
+        }
+        else
+        {
+            Console.WriteLine("la frase no contiene 'fin'");
+            continue;
+        }
+
+    }
+    else if( resp == "n")
+    {
+        Console.WriteLine("Saliendo del programa...");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Ingreso incorrecto. Ingrese 's' o 'n'.");
+        continue;
+    }
+}
+
